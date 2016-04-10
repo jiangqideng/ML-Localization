f29927d1e875bfc03825b39ae781e71c8e353bb3
zzz <zzz@zzz.com>
2016-04-10 10:28:37 +0800

    get initial radio map

--- generate_radio_maps/RFID-DFL/deployRFID.m
+++ generate_radio_maps/RFID-DFL/deployRFID.m
@@ -1,6 +1,10 @@
 function [ readerPosition, tagPosition ] = deployRFID(roomLength, roomWidth)
 %rfidDeployment: deploy RFID readers and tags
 % set roomLength and roomWidth, which must be integer
+    if nargin == 0
+        roomLength = 20;
+        roomWidth = 15;
+    end
     if roomLength ~= fix(roomLength) || roomWidth ~= fix(roomWidth)
         disp('roomLength or roomWidth not integer');
         return;
