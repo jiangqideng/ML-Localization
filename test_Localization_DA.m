ede9c39318e2df24a0082a4b30d5ac979d6571bc
jiang <jiangqideng@gmail.com>
2016-04-15 15:02:24 +0800

    add a test

---
+++ test_Localization_DA.m
@@ -0,0 +1,31 @@
+%Localization Simulation Based on Naive Bayes (Classification)
+clc;
+clear;
+%% Data Preprocessing
+addpath(genpath(pwd));
+load radioMap2--20m-15-6APm;
+% load DFL_radio_map;
+fingerprint = fingerprint + normrnd(0, 3, size(fingerprint)); % add noise
+n = size(fingerprint, 3);%the number of APs
+m = 20000;%the number of collected samples
+[RSSMatrix, gridLabel, x_real, y_real] = fp_collection(fingerprint, n, m);
+disp('data collection done.');
+PCT = 0.7;%percentage
+dataTrain = RSSMatrix(1 : ceil(PCT*m), :);
+labelTrain = gridLabel(1 : ceil(PCT*m), :);
+dataTest = RSSMatrix(ceil(PCT*m) + 1 : end, :);
+labelTest = gridLabel(ceil(PCT*m) + 1 : end, :);
+
+%% discriminant analysis classifier
+model = ClassificationDiscriminant.fit(dataTrain, labelTrain);
+labelPredict = predict(model, dataTest);
+classfication_Accuracy = sum(labelPredict == labelTest) / length(labelTest);
+
+[x, y] = label2xy(labelPredict);
+err = mean(sqrt((x - x_real(ceil(PCT*m) + 1 : end)).^2 + (y - y_real(ceil(PCT*m) + 1 : end)).^2));
+fprintf('(discriminant analysis) Classfication_Accuracy: %f%%\n', classfication_Accuracy);
+fprintf('(discriminant analysis) Localization error: %fm\n', err);
+
+
+
+
